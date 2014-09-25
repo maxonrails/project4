688f68e544cb0cee0bdd0463cfbddadae11fae73
johnptmcdonald <johnptmcdonald@gmail.com>
2014-09-24 22:32:23 -0700

    switched database from SQLite to PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140917223940) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "stocks", force: true do |t|
     t.string   "symbol"
     t.string   "name"
